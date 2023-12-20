93e2605c71530c4157c7958a72a79775f5aa917b
Vicky <126154136+vickyxiz@users.noreply.github.com>
2023-12-20 19:13:45 +0530

    Update 07.print a function.py

--- 07.print a function.py
+++ 07.print a function.py
@@ -1,7 +1,6 @@
 #The included code stub will read an integer, , from STDIN.
 #Without using any string methods, try to print the following:
 
-
 if __name__ == '__main__':
     n = int(input())
     for i in range(n):
